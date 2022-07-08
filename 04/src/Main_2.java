93f1060db2e5f0291051d98c162ee725e62a1681
camila <tec.camiladossantos@gmail.com>
2022-07-08 16:25:59 -0300

    pasta

---
+++ 04/src/Main_2.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Main_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s1, s2, s3;
+        s1 = sc.nextLine();
+        s2 = sc.nextLine();
+        s3 = sc.nextLine();
+        System.out.println("DADOS DIGITADOS:");
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
+        sc.close();
+    }
+}
+//    Exemplo do terminal:Para ler um texto ATÉ A QUEBRA DE LINHA
+//    pedro foi andar de bicicleta
+//
+//
+//        DADOS DIGITADOS:
+//        pedro foi andar de bicicleta
